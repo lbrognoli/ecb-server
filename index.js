d14b3cd015870a45ae32287ee21a48b042983e60
Leonardo <l.brognoli93@gmail.com>
2019-10-14 12:00:51 -0300

    add enable cors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-
+const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-
+app.use(cors())
 //const requestLogger = require('./src/util/requestLogger')
 //app.use(requestLogger)
 
